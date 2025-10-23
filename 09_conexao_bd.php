179ca4a9d0003339f0ceccea677ee7d345437161
henriquedaraujo6 <henrique.d.araujo6@aluno.senai.br>
2025-10-23 14:08:26 -0300

    problema no 10

--- 09_conexao_bd.php
+++ 09_conexao_bd.php
@@ -1,4 +1,28 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "Senai@118";
+    $dbname  =  "exercicio";
 
+    try {
+        // tenta criar uma conexão com o BD 
+        $conn = new mysqli($servername, $username, $password, $dbname) ;
+        
+        if ($conn->connect_error) {
+            throw new Exception("Falha na conexão: " . $conn->connect_error);
+        }
+        echo "Conexão realizada com sucesso.";
+
+    } catch (Exception $e) {
+        echo "Erro ao conectar ao banco de dados."  . $e->getMessage();
+    
+    }
+
+
+     // Exibe uma mensagem de erro amigável
+
+
+?>
 <!-- Para criar o BD -->
 <!-- CREATE DATABASE exercicio; -->
 
